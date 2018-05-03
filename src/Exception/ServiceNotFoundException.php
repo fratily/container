7233860e97e72467a1aa6151033b9b942be219cd
kento-oka <kento.oka@kentoka.com>
2018-05-03 20:13:05 +0900

    コミット忘れ

---
+++ src/Exception/ServiceNotFoundException.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * FratilyPHP Container
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author      Kento Oka <kento.oka@kentoka.com>
+ * @copyright   (c) Kento Oka
+ * @license     MIT
+ * @since       1.0.0
+ */
+namespace Fratily\Container\Exception;
+
+use Psr\Container\NotFoundExceptionInterface;
+
+/**
+ *
+ */
+class ServiceNotFoundException extends \LogicException implements NotFoundExceptionInterface{
+
+    const _MSG  = "Service not found in container.";
+
+    const MSG   = "Service '{id}' is not found in container.";
+
+    private $id;
+
+    /**
+     * Constructor
+     *
+     * @param   string  $message    [optional]
+     * @param   int $code   [optional]
+     * @param   \Throwable  $previous   [optional]
+     */
+    public function __construct(
+        string $message = null,
+        int $code = 0,
+        \Throwable $previous = null
+    ){
+        parent::__construct($message ?? self::_MSG, $code, $previous);
+    }
+
+    /**
+     * 存在しなかったサービス名を取得する
+     *
+     * @return  string|null
+     */
+    public function getId(){
+        return $this->id;
+    }
+
+    /**
+     * 存在しなかったサービス名を登録する
+     *
+     * 登録したサービス名は変更できない。
+     *
+     * @param   string  $id
+     *
+     * @return  bool
+     */
+    public function setId(string $id){
+        $result = $this->id !== null;
+
+        if($result){
+            $this->id       = $id;
+            $this->message  = str_replace("{id}", $this->id, self::MSG);
+        }
+
+        return $result;
+    }
+}
