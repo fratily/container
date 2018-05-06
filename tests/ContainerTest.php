9cb86e7e445adbbc219c26b0a193ec719ccee065
kento-oka <kento.oka@kentoka.com>
2018-05-06 17:01:51 +0900

    デリゲートコンテナとnotfound例外のテスト

--- tests/ContainerTest.php
+++ tests/ContainerTest.php
@@ -14,6 +14,10 @@
 namespace Fratily\Tests\Container;
 
 use Fratily\Container\Container;
+use Psr\Container\{
+    ContainerExceptionInterface,
+    NotFoundExceptionInterface
+};
 
 /**
  *
@@ -34,5 +38,35 @@ class ContainerTest extends \PHPUnit\Framework\TestCase{
         $this->assertFalse($di->has("not_found"));
 
         $this->assertSame($q_v, $di->get($q_k));
+    }
+
+    public function testNotFound(){
+        $this->expectException(NotFoundExceptionInterface::class);
+
+        $di = Container::createInstance();
+
+        $di->get("not_found");
+    }
+
+    public function testGetWithDelegate(){
+        $di     = Container::createInstance();
+        $di2    = Container::createInstance();
+        $queue  = new \SplQueue();
+
+        $di2->set("queue", $queue);
+
+        $di->addDelegateContainer($di2);
+
+        $this->assertSame($queue, $di->get("queue"));
+    }
+
+    public function testNotFoundWithDelegate(){
+        $this->expectException(NotFoundExceptionInterface::class);
+
+        $di     = Container::createInstance();
+
+        $di->addDelegateContainer(Container::createInstance());
+
+        $di->get("not_found");
     }
 }
